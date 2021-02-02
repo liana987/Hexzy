7822a4266c9d2579f4cecb2df6e6f57f1d7a230e
prabhasha-p <74059196+prabhasha-p@users.noreply.github.com>
2021-02-02 13:50:11 +0530

    Hexzy
    A PowerFul Group Management Bot

---
+++ SaitamaRobot/modules/fun_strings.py
@@ -0,0 +1,283 @@
+RUN_STRINGS = (
+    "ට්‍රැක්ටරය පැදවීමට මාගේ ඡායාරූප භාවිත කිරීමෙන් වලකින්න"
+    "කවුරු කොහොම් කිව්වත්  \nකොත්තු කෑවොත් බඩ යන එක යනවමයි!😅",
+    "උඹ නං පාපයක් ඕයි!!!",
+    "ජාත්‍යන්තරේට ආතල් දෙන්නෙපා යකෝ 😅",
+    "සීනි කන්න ආපු කූඹියො නෙමෙයි සීනි බෝතලේ ඇරපු ඔයයි වැරදි..😋",
+    "ඔයා හරි සෝයි අනේ.. සෝ කියුට්... 😋",
+    "පොඩිකාලෙ බිව්වෙ පොල්කිරිද මල්ලී 🤑.",
+    "ජාත්‍යන්තරේට ආතල් දෙන්නෙපා දෙයියො 😅  ",
+    "බය තරහ ඇති කරවයි. \nතරහ වයිරය උපදවයි. \nවරිරය පසුතැවීම ඇති කරයි.\nඔබ බයෙන් ජීවත්වන තුරු ලංකාවේ බඩු මිල පහත නොයයි😅",
+    "තමුසෙ පිස්සෙක්නෙ ඔ්යි 😅",
+    "මැරිලත් දවස් දෙක තුනක් ආදරෙයි 😅",
+    "අපි දෙන්න පැනල යමු.. හාද?? ",
+    "ඔන්න බොගේ සරම ලිහිලා...හූ.... 😂",
+    "ආචි තපර ලාහිලා \nබීචි තපර ලාහිලා \nආචි තපර බීචි තපර ඩිෂ්කු ඩිෂ්කු ලාහිලා 😅",
+    "උඹ නාඩං \nඋඹ නාඩං \nබොගෙ නඩු මට \nකට පාඩං 😆",
+    "😆",
+    "💔මේ  ගෲප් එකත් එක්ක සෙට් වෙලා නැත්තං ලංකාවෙ ඉඳල වැඩකුත් නෑ .\n😋👉 @gangofmafiateam 💔",
+    "චූ කරල නිදාගනින් අයියේ...",
+    "ටොයිලට් එකේ ඉද්දි හෙඩ්සෙට් එක ගහන් සින්දු අහන්න එපා ඕයි...  😅",
+)
+
+GIFS = [
+    'CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE',
+    'CgACAgUAAx0CU_rCTAABAjdgX1s4NVaeCls6YaH3p43vgdCRwQIAAqsAA4P_MFUYQhyoR-kgpRsE',
+    'CgACAgUAAx0CU_rCTAABAjdSX1s3fq5iEJ64YeQLKI8cD7CSuSEAAlUBAAJu09hW5iqWB0hTPD4bBA'
+]
+
+SLAP_SAITAMA_TEMPLATES = (
+    "💞 Hexzy 💕 ඔයාට කනේ පාරක් ගැහුව වල් වැඩ කරන්න ගිය නිසා 😒😂",
+    "මට ගහන්න එපා අයියේ  🥺🥺 \nරිදෙනවනෙ මට  🥺🥺  ",
+    [
+        "දැං ඉතිං විනාඩියක් කට පියං හිටු 😂😂",  # normal reply
+        "මට උඹව  නිශ්ශබ්ද බැරි  නිසා මට කම්මුලට ගහන එක නවත්තන්න 🥺🥺 ",  # reply to admin
+        "tmute",  # command
+        "කට වහං හිටු ",
+        "Silence!"
+    ])
+
+SLAP_TEMPLATES = (
+    "{user2} was killed by magic.",
+    "{user2} starved without pats.",
+    "{user2} was knocked into the void by {user1}.",
+    "{user2} fainted.",
+    "{user2} is out of usable Pokemon! {user2} whited out!.",
+    "{user2} is out of usable Pokemon! {user2} blacked out!.",
+    "{user2} got rekt.",
+    "{user2}'s melon was split by {user1}.",
+    "{user2} was sliced and diced by {user1}.",
+    "{user2} played hot-potato with a grenade.",
+    "{user2} was knifed by {user1}.",
+    "{user2} ate a grenade.",
+    "{user2} is what's for dinner!",
+    "{user2} was terminated by {user1}.",
+    "{user1} spammed {user2}'s email.",
+    "{user1} RSA-encrypted {user2} and deleted the private key.",
+    "{user1} put {user2} in the friendzone.",
+    "{user1} slaps {user2} with a DMCA takedown request!",
+    "{user2} got a house call from Doctor {user1}.",
+    "{user1} beheaded {user2}.",
+    "{user2} got stoned...by an angry mob.",
+    "{user1} sued the pants off {user2}.",
+    "{user2} was one-hit KO'd by {user1}.",
+    "{user1} sent {user2} down the memory hole.",
+    "{user2} was a mistake. - '{user1}' ",
+    "{user2} was made redundant.",
+    "{user1} {hits} {user2} with a bat!.",
+    "{user1} {hits} {user2} with a Taijutsu Kick!.",
+    "{user1} {hits} {user2} with X-Gloves!.",
+    "{user1} {hits} {user2} with a Jet Punch!.",
+    "{user1} {hits} {user2} with a Jet Pistol!.",
+    "{user1} {hits} {user2} with a United States of Smash!.",
+    "{user1} {hits} {user2} with a Detroit Smash!.",
+    "{user1} {hits} {user2} with a Texas Smash!.",
+    "{user1} {hits} {user2} with a California Smash!.",
+    "{user1} {hits} {user2} with a New Hampshire Smash!.",
+    "{user1} {hits} {user2} with a Missouri Smash!.",
+    "{user1} {hits} {user2} with a Carolina Smash!.",
+    "{user1} {hits} {user2} with a King Kong Gun!.",
+    "{user1} {hits} {user2} with a baseball bat - metal one.!.",
+    "*Serious punches {user2}*.",
+    "*Normal punches {user2}*.",
+    "*Consecutive Normal punches {user2}*.",
+    "*Two Handed Consecutive Normal Punches {user2}*.",
+    "*Ignores {user2} to let them die of embarassment*.",
+    "*points at {user2}* What's with this sassy... lost child?.",
+    "*Hits {user2} with a Fire Tornado*.",
+    "{user1} pokes {user2} in the eye !",
+    "{user1} pokes {user2} on the sides!",
+    "{user1} pokes {user2}!",
+    "{user1} pokes {user2} with a needle!",
+    "{user1} pokes {user2} with a pen!",
+    "{user1} pokes {user2} with a stun gun!",
+    "{user2} is secretly a Furry!",
+    "Hey Everybody! {user1} is asking me to be mean!",
+    "( ･_･)ﾉ⌒●~* (･.･;) <-{user2}",
+    "Take this {user2}\n(ﾉﾟДﾟ)ﾉ ))))●~* ",
+    "Here {user2} hold this\n(｀・ω・)つ ●~＊",
+    "( ・_・)ノΞ●~*  {user2}\nDieeeee!!.",
+    "( ・∀・)ｒ鹵~<≪巛;ﾟДﾟ)ﾉ\n*Bug sprays {user2}*.",
+    "( ﾟДﾟ)ﾉ占~<巛巛巛.\n-{user2} You pest!",
+    "( う-´)づ︻╦̵̵̿╤── \(˚☐˚”)/ {user2}.",
+    "{user1} {hits} {user2} with a {item}.",
+    "{user1} {hits} {user2} in the face with a {item}.",
+    "{user1} {hits} {user2} around a bit with a {item}.",
+    "{user1} {throws} a {item} at {user2}.",
+    "{user1} grabs a {item} and {throws} it at {user2}'s face.",
+    "{user1} launches a {item} in {user2}'s general direction.",
+    "{user1} starts slapping {user2} silly with a {item}.",
+    "{user1} pins {user2} down and repeatedly {hits} them with a {item}.",
+    "{user1} grabs up a {item} and {hits} {user2} with it.",
+    "{user1} ties {user2} to a chair and {throws} a {item} at them.",
+    "{user1} gave a friendly push to help {user2} learn to swim in lava.",
+    "{user1} bullied {user2}.",
+    "Nyaan ate {user2}'s leg. *nomnomnom*",
+    "{user1} {throws} a master ball at {user2}, resistance is futile.",
+    "{user1} hits {user2} with an action beam...bbbbbb (ง・ω・)ง ====*",
+    "{user1} ara ara's {user2}.",
+    "{user1} ora ora's {user2}.",
+    "{user1} muda muda's {user2}.",
+    "{user2} was turned into a Jojo reference!",
+    "{user1} hits {user2} with {item}.",
+    "Round 2!..Ready? .. FIGHT!!",
+    "WhoPixel will oof {user2} to infinity and beyond.",
+    "{user2} ate a bat and discovered a new disease.",
+    "{user1} folded {user2} into a paper plane",
+    "{user1} served {user2} some bat soup.",
+    "{user2} was sent to his home, the planet of the apes.",
+    "{user1} kicked {user2} out of a moving train.",
+    "{user2} just killed John Wick’s dog.",
+    "{user1} performed an Avada Kedavra spell on {user2}.",
+    "{user1} subjected {user2} to a fiery furnace.",
+    "Sakura Haruno just got more useful than {user2}",
+    "{user1} unplugged {user2}'s life support.",
+    "{user1} subscribed {user2}' to 5 years of bad internet.",
+    "You know what’s worse than Dad jokes? {user2}!",
+    "{user1} took all of {user2}'s cookies.",
+    "{user2} wa mou.......Shindeiru! - {user1}.",
+    "{user2} lost his race piece!",  #No game no life reference
+    "Shut up {user2}, you are just {user2}.",  #No game no life reference
+    "{user1} hits {user2} with Aka si anse!",  #No game no life reference
+    "@NeoTheKitty scratches {user2}",  #Pixels pet cat - @NeoTheKitty 
+    "Majin buu ate {user2}",  #Dbz
+    "Goblin slayer slays {user2}",  #Goblin Slayer
+)
+
+PAT_TEMPLATES = (
+    "{user1} pats {user2} on the head.",
+    "*gently rubs {user2}'s head*.",
+    "*{user1} mofumofus {user2}'s head*",
+    "*{user1} messes up {user2}'s head*",
+    "*{user1} intensly rubs {user2}'s head*",
+    "*{user2}'s waifu pats their head*",
+    "*{user2}'s got free headpats*",
+    "No pats for {user2}!",
+    "Oh no! We are all out of pats.",
+    "This is a designated no pat zone!",
+    "No pats for {user2}!",
+    "{user1} spoils {user2} with headpats!",
+    "{user2} received one free headpat!",
+    "{user1} headpats {user2} whilst giving a lap pillow",
+    "{user1} aggressively pats {user2}",
+    "{user1} gently strokes {user2}'s head",
+    "Pat, pat, pat, pat",
+    "{user2} could not escape {user1}'s headpats",
+    "{user2}.exe has stopped working",
+    "{user1} rubs {user2} on the neck",
+    "Must... extort... HEADPATS",
+    "{user1} headpats {user2}'s head with a pat",
+    "{user1} pats {user2} unexpectedly",
+    "{user1} pats {user2} with consent, maybe?",
+    "Pat pat, {user2} honto kawaii ne!",
+    "{user1} headpats {user2} at 420apm",
+    "{user1} bellyrubs {user2}",
+    "{user1} pats {user2} friendlily",
+    "{user2} uses HEADPATS? O KAWAII KOTO",
+    "*headpats.gif intensifies for {user2}*",
+    "(*´ω´(*｀ω｀)",
+    "(ｏ・_・)ノ”(ᴗ_ ᴗ。)",
+    "(*￣▽￣)ノ”(- -*)",
+    "(っ´ω`)ﾉ(╥ω╥)",
+    "( ´Д｀)ﾉ(´･ω･`) ﾅﾃﾞﾅﾃﾞ",
+)
+
+PAT_GIFS = (
+    "CgACAgQAAxkBAALRX19Xs7tBdOH1gQwS_rglVRkTbgVYAAKEAgACmQn9UWlyGa_xy9_aGwQ",
+    "CgACAgUAAxkBAAICr2AY9WaVz4nieZfol8681RkNqPP9AAI7AgACUzp5V08yL5PQTH9zHgQ")
+
+PAT_STICKERS = (
+    "CAACAgQAAxkBAALRWV9Xs4HH0XaXfhZe-jWaZoXfs-IsAAJYAwACdDgSEHYOt4KvL02oGwQ",
+    " CAACAgIAAxkBAAICu2AY-KKwZLYZcLPIWKCVCn5i8o8kAAISAwACusCVBeGCYBc2pVzOHgQ")
+
+PING_STRING = (
+    "PONG!!",
+    "I am here!",
+)
+
+ITEMS = (
+    "[🅶🅰️🅽🅶 🅾️🅵 🅼🅰️🅵🅸🅰️ ](t.me/GangOfMafiaTeam)",
+    "[Sri Lanka 🇱🇰❤️](t.me/GangOfMafiaTeam)",
+    "[cricket bat](t.me/GangOfMafiaTeam)",
+    "[wooden cane](t.me/GangOfMafiaTeam)",
+    "[shovel](t.me/GangOfMafiaTeam)",
+    "[toaster](t.me/GangOfMafiaTeam)",
+    "[book](t.me/GangOfMafiaTeam)",
+    "[laptop](t.me/GangOfMafiaTeam)",
+    "[Telegram ](t.me/GangOfMafiaTeam)",
+    "[spiked bat](t.me/GangOfMafiaTeam)",
+    "[heavy rock](t.me/GangOfMafiaTeam)",
+    "[chunk of dirt](t.me/GangOfMafiaTeam)",
+    "[ton of bricks](t.me/GangOfMafiaTeam)",
+    "[rasengan](t.me/GangOfMafiaTeam)",
+    "[spirit bomb](t.me/GangOfMafiaTeam)",
+    "[Join Now](t.me/GangOfMafiaTeam)",
+    "[Telegram 🇱🇰❤️](t.me/GangOfMafiaTeam)",
+    "[Murasame](t.me/GangOfMafiaTeam)",
+    "[ban](t.me/GangOfMafiaTeam)",
+    "[chunchunmaru](t.me/GangOfMafiaTeam)",
+    "[彡(ツ)★彡ᵖⓡ𝓪Ｂ卄𝒶𝐒𝒽ค ـ](t.me/GangOfMafiaTeam)",
+    "[rasengan](t.me/GangOfMafiaTeam)",
+    "[Sri Lanka 🇱🇰❤️](t.me/GangOfMafiaTeam)",
+)
+
+THROW = (
+    "throws",
+    "flings",
+    "chucks",
+    "hurls",
+)
+
+HIT = (
+    "හුකයි ",
+    "කන පලනෝ ",
+    "දත් ටික බඩට යවනෝ ",
+    "තෝ එකක් බොනෝ ",
+    "පොල් කඩනෝ ",
+    "අල්ලයි මගෙ නැට්ට ",
+)
+
+EYES = [['⌐■', '■'], [' ͠°', ' °'], ['⇀', '↼'], ['´• ', ' •`'], ['´', '`'],
+        ['`', '´'], ['ó', 'ò'], ['ò', 'ó'], ['⸌', '⸍'], ['>',
+                                                         '<'], ['Ƹ̵̡', 'Ʒ'],
+        ['ᗒ', 'ᗕ'], ['⟃', '⟄'], ['⪧', '⪦'], ['⪦', '⪧'], ['⪩', '⪨'], ['⪨', '⪩'],
+        ['⪰', '⪯'], ['⫑', '⫒'], ['⨴', '⨵'], ['⩿', '⪀'], ['⩾', '⩽'], ['⩺', '⩹'],
+        ['⩹', '⩺'], ['◥▶', '◀◤'], ['◍', '◎'], ['/͠-', '┐͡-\\'], ['⌣', '⌣”'],
+        [' ͡⎚', ' ͡⎚'], ['≋'], ['૦ઁ'], ['  ͯ'], ['  ͌'], ['ළ'], ['◉'], ['☉'],
+        ['・'], ['▰'], ['ᵔ'], [' ﾟ'], ['□'], ['☼'], ['*'], ['`'], ['⚆'], ['⊜'],
+        ['>'], ['❍'], ['￣'], ['─'], ['✿'], ['•'], ['T'], ['^'], ['ⱺ'], ['@'],
+        ['ȍ'], ['  '], ['  '], ['x'], ['-'], ['$'], ['Ȍ'], ['ʘ'], ['Ꝋ'],
+        [''], ['⸟'], ['๏'], ['ⴲ'], ['◕'], ['◔'], ['✧'], ['■'], ['♥'], [' ͡°'],
+        ['¬'], [' º '], ['⨶'], ['⨱'], ['⏓'], ['⏒'], ['⍜'], ['⍤'], ['ᚖ'], ['ᴗ'],
+        ['ಠ'], ['σ'], ['☯']]
+
+MOUTHS = [['v'], ['ᴥ'], ['ᗝ'], ['Ѡ'], ['ᗜ'], ['Ꮂ'], ['ᨓ'], ['ᨎ'],
+          ['ヮ'], ['╭͜ʖ╮'], [' ͟ل͜'], [' ͜ʖ'], [' ͟ʖ'], [' ʖ̯'], ['ω'], [' ³'],
+          [' ε '], ['﹏'], ['□'], ['ل͜'], ['‿'], ['╭╮'], ['‿‿'], ['▾'], ['‸'],
+          ['Д'], ['∀'], ['!'], ['人'], ['.'], ['ロ'], ['_'], ['෴'], ['ѽ'], ['ഌ'],
+          ['⏠'], ['⏏'], ['⍊'], ['⍘'], ['ツ'], ['益'], ['╭∩╮'], ['Ĺ̯'], ['◡'],
+          [' ͜つ']]
+
+EARS = [['q', 'p'], ['ʢ', 'ʡ'], ['⸮', '?'], ['ʕ', 'ʔ'], ['ᖗ', 'ᖘ'], ['ᕦ', 'ᕥ'],
+        ['ᕦ(', ')ᕥ'], ['ᕙ(', ')ᕗ'], ['ᘳ', 'ᘰ'], ['ᕮ', 'ᕭ'], ['ᕳ', 'ᕲ'],
+        ['(', ')'], ['[', ']'], ['¯\\_', '_/¯'], ['୧', '୨'], ['୨', '୧'],
+        ['⤜(', ')⤏'], ['☞', '☞'], ['ᑫ', 'ᑷ'], ['ᑴ', 'ᑷ'], ['ヽ(', ')ﾉ'],
+        ['\\(', ')/'], ['乁(', ')ㄏ'], ['└[', ']┘'], ['(づ', ')づ'], ['(ง', ')ง'],
+        ['⎝', '⎠'], ['ლ(', 'ლ)'], ['ᕕ(', ')ᕗ'], ['(∩', ')⊃━☆ﾟ.*']]
+
+TOSS = ("Heads", "Tails")
+
+EIGHTBALL = [
+    "🔴 උදාව පසුපස හඹා යන්න, ඔබේ සෙවනැල්ල ඔබ පිටුපසින් සිටිනු ඇත😉", "🟡 වස්තර කෙරෙහි අවධානය යොමු කරන්න  \nඔබට රසවත් යමක් පෙනෙනු ඇත 😋", "🔴 ඔබ කවදාවත් රස නොගත් එම ආහාරය ඇත්තෙන්ම රසවත් විය හැකිය.😋😋",
+    "🔴 සත්ව ඇසුරෙන් වඩාත් කේන්තියෙන් යුත් මුහුණට පවා සිනහවක් ගෙන එනු ඇත ️😔 🐶 😊", "🟡 වශිෂ්ට තෑග්ගක් බලාපොරොත්තුවෙන් සිටී  \nනමුත් සාධාරණ අනතුරු ඇඟවීමක්, \n එය ඉතා ඉවසිලිවන්ත තෑග්ගක් 👀",
+    "🟡 ඔබගේ ප්‍රියතම යූ ටියුබ් වීඩියෝව තවම සොයාගෙන නැත.😕", "🟢 ඔබ අවධානය යොමු කළහොත් අවස්ථාවක් ලැබෙනු ඇත 👀", "🟡 විස්තර කෙරෙහි අවධානය යොමු කරන්න  \ nඔබට රසවත් යමක් පෙනෙනු ඇත 😋",
+    "🟡 පිරිමියෙකු නිරුවත් වූ විට ඔහුට හෙම්බිරිස්සාව වැලඳෙයි \nගැහැනියක නිරුවත් වූ විට ඇයට පිරිමියෙකු සම්පාදනය වේ ", "🔴 කවියො කවි ලීවට......\nවෙනස් වේවිද මේ මිනිසුන්ගෙ සිත......", "🔴 හිතින් යන අය..\n අතින් අල්ලා..\nනවත්තන්නට බෑ.....😒",
+    "🔴 සබඳකම් \nරඳා පවතින්නේ \nවගකීම් මත නොව \nයුතුකම් හා දයාබරත්වය මතයි 🙂 ", "🟢 දින \nඅමතක වේවි.... \nනමුත් \nමතකයන් \n අමකත නොවේවි........", "🟡 පොල් අතු වැටෙනකොට.... \nගොක් අතු හිනැහෙනවා.... \nගොක් අතු උවත් දවසක...... \nපොල් අතු වෙනවා.....",
+    "🟢 නොබලන් කුමරිය... \nනිසසල මා දෙස.... \nසසල ඔබේ දෙනෙතින්.....", "🔴 බැඳීමක වටිනාකම දන්නෙ \nඒ බැඳීම් එක්ක \nදවසක් හරි ජීවත් උන \n මනුස්සයෝ විතරයි  🙂", "🟢 Without a doubt.", "🟢 Yes.",
+    "🟢 ශක්තිමත් අරමුනක් තියෙන \nකිසිවෙකු දුර්වල කිරීමට \nමෙලොව කිසි දෙයකට නොහැක .", "🟢 හොඳින් සෙවිලි කළ ගෙය \nවැස්සට නොතෙමෙන්නාක් මෙන් \nමනාව භාවනාවට හුරු පුරුදු \nකරගන්නා සිත් \nකිසිදා රාගයෙන් නොඇලේ..."
+]
+
+DECIDE = ("ඔව්  ✅.", "නෑ  ✖️", "වෙන්නත් පුලුවං ඉතිං 😉")
+
+TABLE = ("t.me/gangOfMafiaTeam", "t.me/Mafia_Update",
+         "🎨 එකතු වෙන්න අප සමඟත් \n 👉🛡 @GangOfMafiaTeam 🙂 \n 👉🛡 @Mafia_Update 🙂 ")
